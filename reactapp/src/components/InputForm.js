99fa4020d033bf3bafbd86f37286eeb688a67dd0
Maryia Klimkovich <maryia.klimkovich@danfoss.com>
2023-07-05 10:58:35 +0300

    Implemented validation in input field. Added callback to handle valid state in parent component.

--- reactapp/src/components/InputForm.js
+++ reactapp/src/components/InputForm.js
@@ -1,12 +1,42 @@
+import { useState } from 'react';
 import { Form, FormGroup } from 'react-bootstrap';
 
-export default function InputForm () {
+export default function InputForm({ formId, onValidationSucceed, disabled }) {
+    const [value, setValue] = useState('');
+    const [validated, setValidated] = useState(false);
+
+    const onInput = (e) => setValue(e.target.value);
+
+    const onHandleSubmit = (event) => {
+      const form = event.currentTarget;
+
+      event.stopPropagation();
+      event.preventDefault();
+
+      if (form.checkValidity() === true) {
+        onValidationSucceed(value);
+        setValidated(undefined);
+      }    
+      else
+      {
+        setValidated(true);
+      }
+      
+      setValue(value);
+    }
+
     return (
         <>
-            <FormGroup controlId="inputArea">
-                <Form.Control as="textarea" rows={10} placeholder="Enter text here..." />
-                <Form.Text id="validationError" muted></Form.Text>
-            </FormGroup>
+            <Form id={formId} noValidate validated={validated} onSubmit={onHandleSubmit}>                                 
+                <FormGroup>
+                    <Form.Control value={value} onInput={onInput}
+                                  disabled={disabled} as="textarea" rows={10}
+                                  placeholder="Enter text here..." required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide text for encoding.
+                    </Form.Control.Feedback>
+                </FormGroup>
+            </Form>   
         </>
     );
 }
